788ced8067926ac72e4bcdd16d435e51df32474f
Albertomomu <alberto.moreno2357@gmail.com>
2023-03-04 21:56:12 +0100

    creating home component & routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: HomeComponent},
+  { path: 'films', loadChildren: () => import('./pages/films/films.module').then(m => m.FilmsModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
