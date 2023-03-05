46a3d96a955c96bf42503cb204242fb39293eb6f
Albertomomu <alberto.moreno2357@gmail.com>
2023-03-05 13:14:03 +0100

    feat: updated menu items list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,6 @@ import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
   {path:'', component: HomeComponent},
-  { path: 'films', loadChildren: () => import('./pages/films/films.module').then(m => m.FilmsModule) }
 ];
 
 @NgModule({
